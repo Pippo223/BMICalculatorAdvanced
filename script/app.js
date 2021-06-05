867e01c1777252c7670527b435950a3db7bf43dd
Philip Owusu Asare <philip.asare@amalitech.org>
2021-06-05 19:05:30 +0000

    Error handling to a large extent

--- script/app.js
+++ script/app.js
@@ -1,29 +1,31 @@
 
 function BMICalculator (w, h) {
 
-  const BMI = w / Math.pow (h, 2);
- 
-  if (BMI > 0 && BMI < 18.5)    
-  return ("Your BMI is " + BMI + " so you are underweight.")
+  const BMI_Long = w / Math.pow (h, 2);
+  const BMI = BMI_Long.toFixed(2); 
   
-  else if (BMI >= 18.5 && BMI <= 24.9)
-  return ("Your BMI is " + BMI + " so you have a normal weight.")
+  return  BMI == Infinity ?  " Height cannot be 0 or empty !!!" 
 
-  else if (BMI > 24.9)
-  return ("Your BMI is " + BMI + " so you are overweight.");
-   
-   else 
-  return ("Input format not supported") 
+    : BMI < 0 ? "Negative numbers are not allowed !!!"
+  
+    : BMI > 0 && BMI < 18.5 ? "Your BMI is " + BMI + ", so you are underweight!"
+    
+    : BMI >= 18.5 && BMI <= 24.9? "Your BMI is " + BMI + ", so you have a normal weight."
+
+    : BMI > 24.9 ? "Your BMI is " + BMI + ", so you are overweight!!"
+
+    : BMI == 0.00 ? "Weight cannot be 0 or empty !!!"
+
+    :  " Both fields must have input !!!"
+  
 }
-
-
 
 
 function displayBMI() {
   const Weight = document.getElementById("weight").value;
   const Height = document.getElementById("height").value;
+  
   const bmi1 = BMICalculator(Weight,Height)
   document.getElementById("result").innerHTML = bmi1; 
- 
 
 }
